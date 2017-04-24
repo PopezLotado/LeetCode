855184ed0c97a592a3b961d0e8ec468577bd798a
PopezLotado <admin@pzone.org>
2017-04-25 00:16:02 +0800

    0425

--- src/medium/T16.java
+++ src/medium/T16.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.Arrays;
+
 /**
  * @Title 3Sum Closest
  * @Description Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
@@ -17,6 +19,29 @@ public class T16 {
 	}
 
     public int threeSumClosest(int[] nums, int target) {
-        return 0;
+    	int res=0;
+        if(nums.length<=3){
+        	for(int i=0;i<nums.length;i++)
+        		res+=nums[i];
+        	return res;
+        }
+        Arrays.sort(nums);
+        res=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<nums.length-2;i++){
+        	int l=i+1;
+        	int h=nums.length-1;
+        	while(l<h){
+        		int t=nums[i]+nums[l]+nums[h];
+        		if(Math.abs(target-t) < Math.abs(target-res))
+        			res=t;
+        		if(t==target)
+        			return t;
+        		if(t>target)
+        			h--;
+        		else
+        			l++;
+        	}
+        }
+        return res;
     }
 }
